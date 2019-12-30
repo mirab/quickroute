f3b72916f3602118aa0e6dec74def793ec557689
Miroslav Benes <mbenes@suse.cz>
2019-12-30 13:29:37 +0100

    Avoid an exception in QuickRoute.UI.Main.CalculateLapInfo()
    CalculateLapInfo() sets RowCount and ColumnCount properties for the
    internal DataGridView component (laps property). RowCount is set first.
    Unfortunately, mono implementation of the setter (set_RowCount method)
    throws an exception. ColumnCount is set to 0 at this point, VirtualMode
    is true, and thus UpdateRowHeightInfo() method is called with rowIndex
    parameter being 0 right after one artificial column is created.
    UpdateRowHeightInfo() throws the exception because rowIndex ==
    Rows.Count. See [1] for reference.
    
    Avoid the exception by setting ColumnCount property first in
    CalculateLapInfo().
    
    Note: this is just a work around for mono bug in my opinion. Original
    Windows implementation of .NET does not have the issue. It creates the
    artificial column too and then continues with adding the rows [2].
    
    [1] https://github.com/mono/mono/blob/master/mcs/class/System.Windows.Forms/System.Windows.Forms/DataGridView.cs
    [2] https://source.dot.net/#System.Windows.Forms/System/Windows/Forms/DataGridView.cs
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>

--- QuickRoute.UI/Main.cs
+++ QuickRoute.UI/Main.cs
@@ -1377,10 +1377,8 @@ namespace QuickRoute.UI
 
       // set number of rows and columns in grid
       updatingUINowCounter++;
-      laps.RowCount = 0;
-      laps.ColumnCount = 0;
-      laps.RowCount = lapInfoList.Count;
       laps.ColumnCount = (lapInfoList.Count == 0 ? 0 : lapInfoList[0].GetProperties().Count);
+      laps.RowCount = lapInfoList.Count;
       updatingUINowCounter--;
       SetLapGridHeaders();
       SortLapGrid();
