024ac1528840ea281d36322f451fc21bcb92dd7c
Miroslav Benes <mbenes@suse.cz>
2019-12-30 14:01:30 +0100

    Replace PathCompactPathEx() with a native approach
    PathCompactPathEx() is WinAPI function used for shortening of paths. As
    such it is not supported by Mono. Replace it with a native approach
    using only standard .NET features. Based on [1].
    
    [1] http://www.trsdomain.dk/blog/2011/1/9/shortening-file-paths---c-sharp-function.html
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>

--- QuickRoute.UI/Classes/Util.cs
+++ QuickRoute.UI/Classes/Util.cs
@@ -127,11 +127,84 @@ namespace QuickRoute.UI.Classes
       }
     }
 
-    public static string PathShortener(string path, int length)
+    public static string PathShortener(string path, int maxLength)
     {
-      var sb = new StringBuilder();
-      PathCompactPathEx(sb, path, length, 0);
-      return sb.ToString();
+      string ellipsisChars = "...";
+      char dirSeparatorChar = Path.DirectorySeparatorChar;
+      string directorySeparator = dirSeparatorChar.ToString();
+
+      if (path.Length <= maxLength)
+      {
+        return path;
+      }
+      int ellipsisLength = ellipsisChars.Length;
+      if (maxLength <= ellipsisLength)
+      {
+        return ellipsisChars;
+      }
+
+      //alternate between taking a section from the start (firstPart) or the path and the end (lastPart)
+      bool isFirstPartsTurn = true; //drive letter has first priority, so start with that and see what else there is room for
+      //vars for accumulating the first and last parts of the final shortened path
+      string firstPart = "";
+      string lastPart = "";
+      //keeping track of how many first/last parts have already been added to the shortened path
+      int firstPartsUsed = 0;
+      int lastPartsUsed = 0;
+
+      string[] pathParts = path.Split(dirSeparatorChar);
+      for (int i = 0; i < pathParts.Length; i++)
+      {
+        if (isFirstPartsTurn)
+        {
+            string partToAdd = pathParts[firstPartsUsed] + directorySeparator;
+            if ((firstPart.Length + lastPart.Length + partToAdd.Length + ellipsisLength) > maxLength)
+            {
+                break;
+            }
+            firstPart = firstPart + partToAdd;
+            if (partToAdd == directorySeparator)
+            {
+                //this is most likely the first part of and UNC or relative path
+                //do not switch to lastpart, as these are not "true" directory seperators
+                //otherwise "\\myserver\theshare\outproject\www_project\file.txt" becomes "\\...\www_project\file.txt" instead of the intended "\\myserver\...\file.txt")
+            }
+            else
+            {
+                isFirstPartsTurn = false;
+            }
+            firstPartsUsed++;
+        }
+        else
+        {
+            int index = pathParts.Length - lastPartsUsed - 1; //-1 because of length vs. zero-based indexing
+            string partToAdd = directorySeparator + pathParts[index];
+            if ((firstPart.Length + lastPart.Length + partToAdd.Length + ellipsisLength) > maxLength)
+            {
+                break;
+            }
+            lastPart = partToAdd + lastPart;
+            if (partToAdd == directorySeparator)
+            {
+                //this is most likely the last part of a relative path (e.g. "\websites\myproject\www_myproj\App_Data\")
+                //do not proceed to processing firstPart yet
+            }
+            else
+            {
+                isFirstPartsTurn = true;
+            }
+            lastPartsUsed++;
+        }
+      }
+
+      if (lastPart == "")
+      {
+        //the filename (and root path) in itself was longer than maxLength, shorten it
+        lastPart = pathParts[pathParts.Length - 1];//"pathParts[pathParts.Length -1]" is the equivalent of "Path.GetFileName(pathToShorten)"
+        lastPart = lastPart.Substring(lastPart.Length + ellipsisLength + firstPart.Length - maxLength, maxLength - ellipsisLength - firstPart.Length);
+      }
+
+      return firstPart + ellipsisChars + lastPart;
     }
 
     public static void InsertIntoRecentDocumentsList(string fileName)
